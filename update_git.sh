a9fb095f53f25562945aafe8afa845baaf2f2c6f
hmghaly <hmghaly@gmail.com>
2020-08-07 18:48:53 -0400

    Update existing file

--- update_git.sh
+++ update_git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "${DIR}"
+cd "${DIR}"
 git add .
 git commit -m "Update existing file"
 git push --mirror git@github.com:hmghaly/word_align.git
