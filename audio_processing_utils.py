e63fc7fce6f94b4a56ccac3dcf1898aa862a5e09
Hussein Ghaly <hmghaly@users.noreply.github.com>
2022-10-08 10:28:19 -0400

    Add files via upload

--- audio_processing_utils.py
+++ audio_processing_utils.py
@@ -3,6 +3,8 @@
 from pydub import AudioSegment
 
 def split_audio_by_aligned_words(wav_fpath0,out_fpath0,t1,t2): #time in milliseconds
+    t1 = t1 * 1000 #Works in milliseconds
+    t2 = t2 * 1000
     newAudio = AudioSegment.from_wav(wav_fpath0)
     newAudio = newAudio[t1:t2]
     newAudio.export(out_fpath0, format="wav") #Exports to a wav file in the current path.
