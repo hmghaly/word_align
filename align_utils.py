ec8896a1196ddc636a17dc2c72b4de1a852f6e8a
Hussein Ghaly <hmghaly@users.noreply.github.com>
2022-11-20 17:28:46 -0500

    removed ortho

--- align_utils.py
+++ align_utils.py
@@ -229,7 +229,7 @@ def get_rec_el_children(el0,el_child_dict0,el_list0=[],only_without_children=Fal
   return el_list0
 
 
-def get_ne_se_dict(all_elements0): #elements are pairs of src/trg spans
+def get_ne_se_dict(all_elements0,allow_ortho=False): #elements are pairs of src/trg spans - allow othogonal - vertical horizontal spans
   se_transition_dict0,ne_transition_dict0={},{} #identify the possible transitions from one element to another - southeast or northeast
   #test_transition_dict0={}
   for el0,el_wt0 in all_elements0:
@@ -254,12 +254,12 @@ def get_ne_se_dict(all_elements0): #elements are pairs of src/trg spans
       if src_span1==src_span0 and trg_span1[0]>trg_span0[1] and src_span0[1]-src_span0[0]<2: #vertical: multiple target words/phrase corresponding to one source phrase
         direction="south"
         #se_transition_dict0[el0][el1]=el_wt0
-        ne_transition_dict0[el0][el1]=el_wt0
+        if allow_ortho: ne_transition_dict0[el0][el1]=el_wt0
       if trg_span1==trg_span0 and src_span1[0]>src_span0[1] and trg_span0[1]-trg_span0[0]<2: #horizontal: multiple source words/phrases corresponding to one target phrase
         direction="east"
         #ne_list.append((el1,el_wt1))
         #se_transition_dict0[el0][el1]=el_wt0
-        ne_transition_dict0[el0][el1]=el_wt0
+        if allow_ortho: ne_transition_dict0[el0][el1]=el_wt0
 
       if direction==None: continue
   return se_transition_dict0, ne_transition_dict0
