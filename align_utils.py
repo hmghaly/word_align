edd026c1602a61c024c1e17a37381d3809673c10
Hussein Ghaly <hmghaly@users.noreply.github.com>
2022-12-20 15:49:07 -0500

    Add files via upload

--- align_utils.py
+++ align_utils.py
@@ -26,15 +26,17 @@ def filter_toks(tok_list0,params={}):
   return tok_list0
 
 #Most important functions
-def get_index_matching_list(src_tokens0,trg_tokens0,src_index0,trg_index0): #match src/trg phrase based on index matching intersection/ratio
+def get_index_matching_list(src_tokens0,trg_tokens0,src_index0,trg_index0,skip_identical=True): #match src/trg phrase based on index matching intersection/ratio
   src_phrase_index_loc_dict=retr_sent_phrase_indexes(src_tokens0,src_index0)
   trg_phrase_index_loc_dict=retr_sent_phrase_indexes(trg_tokens0,trg_index0)  
   matching_list=[]
   for src_phrase0,src_locs_indexes0 in src_phrase_index_loc_dict.items():
     src_locs0,src_indexes0=src_locs_indexes0
     for trg_phrase0,trg_locs_indexes0 in trg_phrase_index_loc_dict.items():
-      trg_locs0,trg_indexes0=trg_locs_indexes0
-      ratio1,intersection1=get_src_trg_intersection(src_indexes0,trg_indexes0)
+      if skip_identical and src_phrase0==trg_phrase0: ratio1,intersection1=0.5,100 #we do not match indexes for identical phrases, but assign them arbitrary values
+      else:
+        trg_locs0,trg_indexes0=trg_locs_indexes0
+        ratio1,intersection1=get_src_trg_intersection(src_indexes0,trg_indexes0)
       #print("src_phrase0",src_phrase0,"trg_phrase0",trg_phrase0,round(ratio1,4),intersection1)
       matching_list.append((src_phrase0,trg_phrase0,src_locs0,trg_locs0,intersection1,round(ratio1,4)))
 
