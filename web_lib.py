36087ed7c594feac45b519d19e0d2a8000022d30
Hussein Ghaly <hmghaly@users.noreply.github.com>
2022-12-22 17:01:07 -0500

    Add files via upload

--- web_lib.py
+++ web_lib.py
@@ -98,7 +98,7 @@ class DOM:
           
             
           open_tags=open_tags[:-1]
-        else: print("open_tags",open_tags, "tag_name",tag_name)
+        else: print("open_tags",open_tags, "tag_name",tag_name,"tag_str",tag_str)
       else:
         self.tag_id_list.append(assigned_tag_id)
         cur_el=element()
