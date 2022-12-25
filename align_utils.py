d94f2b3db74134ad70edcd255d2e6255ef849570
Hussein Ghaly <hmghaly@users.noreply.github.com>
2022-12-25 15:34:57 -0500

    Add files via upload

--- align_utils.py
+++ align_utils.py
@@ -55,8 +55,8 @@ def get_index_matching(src_tokens0,trg_tokens0,src_index0,trg_index0,max_phrase_
     # trg_check=trg_used_counter_dict.get(trg_phrase0,len(trg_locs0))
     src_check=src_used_counter_dict.get(src_phrase0)
     trg_check=trg_used_counter_dict.get(trg_phrase0)
-    if src_check==None or trg_check==None: #either phrase is the top phrase/not used before
-        if not a in final_matching_list: final_matching_list.append(a)
+    # if src_check==None or trg_check==None: #either phrase is the top phrase/not used before
+    #     if not a in final_matching_list: final_matching_list.append(a)
     if src_check==None: src_check=len(src_locs0)
     if trg_check==None: trg_check=len(trg_locs0)
 
