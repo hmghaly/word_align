195bd4ea3e58716f6fd6869852d698ba4e81fadf
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-02-01 15:04:04 -0500

    Add files via upload

--- file_utils.py
+++ file_utils.py
@@ -66,3 +66,52 @@ def insert_sorted(str0,fpath0,line_size=100): #insert string in a file to mainta
   fopen.close()
   write_fopen.close()
   return output
+
+def inc_count_items(items,fpath): #add items to a text file, indicate their count, and increment each time an item is add it, and move it up the list
+  item_count_dict={}
+  used_counter=0
+  for it0 in items: item_count_dict[it0]=1
+  in_fpath=fpath
+  tmp = tempfile.NamedTemporaryFile(delete=False)
+  tmp_fpath=tmp.name
+  if not os.path.exists(in_fpath):
+    in_fopen=open(in_fpath,"w")
+    for it0 in items:
+      line0="%s\t%s\n"%(it0,1)
+      in_fopen.write(line0)
+    in_fopen.close()
+  else:
+    in_fopen=open(in_fpath)
+    n_lines=0
+    for line0 in in_fopen:
+      n_lines+=1
+      split0=line0.strip().split("\t")
+      key0,count_str0=split0
+      if key0 in items:
+        item_count_dict[key0]=int(count_str0)+1
+        used_counter+=1
+        if used_counter==len(items): break
+    in_fopen.close()
+    items_count_list=sorted(list(item_count_dict.items()),key=lambda x:-x[1])
+    out_fopen=open(tmp_fpath,"w")
+    in_fopen=open(in_fpath)
+    for i0,line0 in enumerate(in_fopen):
+      applied_items_list=[]
+      split0=line0.strip().split("\t")
+      key0,count_str0=split0
+      line_count0=int(count_str0)
+      for item_count1 in items_count_list:
+        item1,count1=item_count1
+        if count1>=line_count0:
+          new_line0="%s\t%s\n"%(item1,count1)
+          out_fopen.write(new_line0)
+          applied_items_list.append(item1)
+      items_count_list=items_count_list[len(applied_items_list):]
+      if not key0 in items: out_fopen.write(line0)
+    for item_count1 in items_count_list:
+      item1,count1=item_count1
+      new_line0="%s\t%s\n"%(item1,count1)
+      out_fopen.write(new_line0)
+    in_fopen.close()
+    out_fopen.close()
+  if os.path.exists(tmp_fpath):shutil.move(tmp_fpath,in_fpath)  
