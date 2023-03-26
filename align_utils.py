dc7e9f6c908d57d3c1f89750d0a3d3ea5dde76a1
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-03-26 15:18:05 -0400

    Add files via upload

--- align_utils.py
+++ align_utils.py
@@ -937,19 +937,28 @@ def match_keyed_dict(src_tokens,trg_tokens,keyed_dict): #match unigram/bigramed 
         all_matching_list.append((src_phrase0,trg_phrase0,src_span0,trg_span0,freq0,wt0))
   return all_matching_list
 
-def qa_keyed_dict(src_tokens,trg_tokens,keyed_dict,filter_params={}): #inspect if for a keyed dict, each phrase found in source have corresponding trg phrase
+
+def qa_keyed_dict(src_tokens,trg_tokens,keyed_dict,dict_type="",filter_params={}): #inspect if for a keyed dict, each phrase found in source have corresponding trg phrase
   all_qa_list=[]
+  #print("testing")
   src_tokens=filter_toks(src_tokens,filter_params)
   trg_tokens=filter_toks(trg_tokens,filter_params)
   src_unigrams_bigrams=get_unigrams_bigrams(src_tokens)
   for ub in src_unigrams_bigrams:
+    #print(ub)
     corr_dict=keyed_dict.get(ub)
+    #print(corr_dict)
     if corr_dict==None: continue
     for src_phrase0,corr_trg_vals in corr_dict.items(): #phrase is a token-ready string
+      #print(src_phrase0)
       src_phrase_tokens0=src_phrase0.split()
       src_phrase_tokens0=filter_toks(src_phrase_tokens0,filter_params)
       src_span0=general.is_in(src_phrase_tokens0,src_tokens)
-      if not src_span0: continue
+      new_src_span=[]
+      for ss in src_span0:
+        start0,end0=ss
+        if end0-start0==len(src_phrase_tokens0)-1: new_src_span.append(ss)
+      if not new_src_span: continue
       #the src phrase exists
       corr_trg_vals.append((src_phrase0,0)) #to account for situations where the corresponding term is also in English
       trg_corr=[]
@@ -958,11 +967,42 @@ def qa_keyed_dict(src_tokens,trg_tokens,keyed_dict,filter_params={}): #inspect i
         trg_phrase_tokens0=filter_toks(trg_phrase_tokens0,filter_params)
         trg_span0=general.is_in(trg_phrase_tokens0,trg_tokens)
         trg_corr.append((trg_phrase0,trg_span0))
-      all_qa_list.append((src_phrase0,src_span0,trg_corr))
+      all_qa_list.append((src_phrase0,src_span0,dict_type,trg_corr))
         # if not trg_span0: continue
         # freq0,wt0=trg_val0
         # all_matching_list.append((src_phrase0,trg_phrase0,src_span0,trg_span0,freq0,wt0))
   return all_qa_list
+
+# def qa_keyed_dict(src_tokens,trg_tokens,keyed_dict,filter_params={}): #inspect if for a keyed dict, each phrase found in source have corresponding trg phrase
+#   all_qa_list=[]
+#   src_tokens=filter_toks(src_tokens,filter_params)
+#   trg_tokens=filter_toks(trg_tokens,filter_params)
+#   src_unigrams_bigrams=get_unigrams_bigrams(src_tokens)
+#   for ub in src_unigrams_bigrams:
+#     corr_dict=keyed_dict.get(ub)
+#     if corr_dict==None: continue
+#     for src_phrase0,corr_trg_vals in corr_dict.items(): #phrase is a token-ready string
+#       src_phrase_tokens0=src_phrase0.split()
+#       src_phrase_tokens0=filter_toks(src_phrase_tokens0,filter_params)
+#       src_span0=general.is_in(src_phrase_tokens0,src_tokens)
+#       new_src_span=[]
+#       for ss in src_span0:
+#         start0,end0=ss
+#         if end0-start0==len(src_phrase_tokens0)-1: new_src_span.append(ss)
+#       if not new_src_span: continue
+#       #the src phrase exists
+#       corr_trg_vals.append((src_phrase0,0)) #to account for situations where the corresponding term is also in English
+#       trg_corr=[]
+#       for trg_phrase0,trg_val0 in corr_trg_vals:
+#         trg_phrase_tokens0=trg_phrase0.split()
+#         trg_phrase_tokens0=filter_toks(trg_phrase_tokens0,filter_params)
+#         trg_span0=general.is_in(trg_phrase_tokens0,trg_tokens)
+#         trg_corr.append((trg_phrase0,trg_span0))
+#       all_qa_list.append((src_phrase0,src_span0,trg_corr))
+#         # if not trg_span0: continue
+#         # freq0,wt0=trg_val0
+#         # all_matching_list.append((src_phrase0,trg_phrase0,src_span0,trg_span0,freq0,wt0))
+#   return all_qa_list
 
 
 #29 Dec 2022
@@ -1023,6 +1063,7 @@ def get_exact_matching(src_toks,trg_toks,default_ratio=0.75):
 #Supporting/helper functions
 #16 Dec 2022
 def add_padding(token_list):
+  if token_list[0]=="<s>" and token_list[-1]=="</s>": return token_list
   return ["<s>"]+token_list+["</s>"]
 
 
