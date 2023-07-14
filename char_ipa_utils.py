220bb1aae79ef57fd2da2bab5e04dde6258131e6
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-07-14 15:20:59 -0400

    Add files via upload

--- char_ipa_utils.py
+++ char_ipa_utils.py
@@ -88,6 +88,184 @@ def word2ipa(word0):
 ar_shape_dict={'أ': ['أ', 'ـأ'], 'ب': ['ب', 'بـ', 'ـبـ', 'ـب'], 'ت': ['ت', 'تـ', 'ـتـ', 'ـت'], 'ث': ['ث', 'ثـ', 'ـثـ', 'ـث'], 'ج': ['ج', 'جـ', 'ـجـ', 'ـج'], 'ح': ['ح', 'حـ', 'ـحـ', 'ـح'], 'خ': ['خ', 'خـ', 'ـخـ', 'ـخ'], 'د': ['د', 'ـد'], 'ذ': ['ذ', 'ـذ'], 'ر': ['ر', 'ـر'], 'ز': ['ز', 'ـز'], 'س': ['س', 'سـ', 'ـسـ', 'ـس'], 'ش': ['ش', 'شـ', 'ـشـ', 'ـش'], 'ص': ['ص', 'صـ', 'ـصـ', 'ـص'], 'ض': ['ض', 'ضـ', 'ـضـ', 'ـض'], 'ط': ['ط', 'طـ', 'ـطـ', 'ـط'], 'ظ': ['ظ', 'ظـ', 'ـظـ', 'ـظ'], 'ع': ['ع', 'عـ', 'ـعـ', 'ـع'], 'غ': ['غ', 'غـ', 'ـغـ', 'ـغ'], 'ف': ['ف', 'فـ', 'ـفـ', 'ـف'], 'ق': ['ق', 'قـ', 'ـقـ', 'ـق'], 'ك': ['ك', 'كـ', 'ـكـ', 'ـك'], 'ل': ['ل', 'لـ', 'ـلـ', 'ـل'], 'م': ['م', 'مـ', 'ـمـ', 'ـم'], 'ن': ['ن', 'نـ', 'ـنـ', 'ـن'], 'هـ': ['ه', 'هـ', 'ـهـ', 'ـه'], 'و': ['و', 'ـو'], 'ي': ['ي', 'يـ', 'ـيـ', 'ـي'], 'ء': ['ء'], 'لا': ['لا', 'ـلا'], 'ئ': ['ئ', 'ئـ', 'ـئـ', 'ـئ'], 'ؤ': ['ؤ', 'ـؤ'], 'ة': ['ة', 'ـة'], 'ى': ['ى', 'ـى'], 'إ': ['إ', 'ـإ'], 'آ': ['آ', 'ـآ'], 'لأ': ['لأ', 'ـلأ'], 'لإ': ['لإ', 'ـلإ'], 'لآ': ['لآ', 'ـلآ']}    
 romanization_dict={'أ': ('2', 'a'), 'ا': ('aa', 'a'), 'إ': ('2i', 'i'), 'آ': ('2aa', 'aa'), 'ى': ('aa', ''), 'ب': ('b', ''), 'ت': ('t', ''), 'ث': ('th', ''), 'ج': ('j', ''), 'ح': ('7', ''), 'خ': ('kh', ''), 'د': ('d', ''), 'ذ': ('dh', ''), 'ر': ('r', ''), 'ز': ('z', ''), 'س': ('s', ''), 'ش': ('sh', ''), 'ص': ('S', ''), 'ض': ('D', ''), 'ط': ('T', ''), 'ظ': ('DH', ''), 'ع': ('3', ''), 'غ': ('gh', ''), 'ف': ('f', ''), 'ق': ('q', ''), 'ك': ('k', ''), 'ل': ('l', ''), 'م': ('m', ''), 'ن': ('n', ''), 'ه': ('h', ''), 'و': ('oo', 'w'), 'ي': ('ee', 'y'), 'ة': ('h', ''), 'ئ': ('2', ''), 'ؤ': ('2', ''), 'ء': ('2', ''), 'َ': ('a', ''), 'ِ': ('i', ''), 'ُ': ('u', ''), 'ٌ': ('un', ''), 'ً': ('an', ''), 'ٍ': ('in', ''), 'ّ': ('x', '')}
 
+# class analyze_ar_text(object):
+#   """docstring for analyze_ar_text"""
+#   def __init__(self, arg):
+# #     super(analyze_ar_text, self).__init__()
+#     self.arg = arg
+    
+
+# class analyze_ar_word: #analyze a word with diacritics in different ways
+#   def __init__(self,text0,shape_dict=ar_shape_dict,romanize_dict=romanization_dict,lang="msa",exclude_waaw_jamaa3ah=True):
+#     all_words=text0.split(" ")
+#     self.text=" ".join([v.split("_")[0] for v in all_words]) 
+#     self.chunks=[]
+#     self.word_letter_shapes_plain=[]
+#     self.word_letter_shapes=[]
+#     self.romanized_chunks=[]
+#     self.romanized=""
+#     self.plain=""
+
+#     for w_i,word0 in enumerate(all_words):
+#       word_split=word0.split("_")
+#       self.word=word_split[0]
+#       self.word_params=""
+#       if len(word_split)>1: self.word_params=word_split[1]
+#       self.word1=word_split[0]
+#       if exclude_waaw_jamaa3ah and self.word1.endswith("وا"):self.word1=self.word1[:-1]
+#       self.shape_dict=shape_dict
+#       self.romanize_dict=romanize_dict
+#       if lang=="ega": self.romanize_dict["ج"]=("g","")
+#       #first split the word into chunks, based on diacritics
+      
+#       new_chunk=""
+      
+#       for w0 in self.word1:
+#         if w0.isalpha() or w0==" ":
+#           self.plain+=w0
+#           if  new_chunk!="": self.chunks.append(new_chunk)
+#           new_chunk=w0 
+#         else: new_chunk+=w0
+#       if  new_chunk!="": self.chunks.append(new_chunk)
+#       #get the shape
+#       prev_shapes=[]
+      
+#       #get the shapes
+#       for i0,ch in enumerate(self.chunks):
+#         att_before,att_after=False,False #attach before and attach after
+#         w0=ch[0]
+#         cur_shape=w0
+#         possible_shapes=shape_dict.get(w0,[])
+
+#         if i0==0 and len(possible_shapes)>2: att_after=True #cur_shape=w0+"ـ"
+#         elif i0==len(self.chunks)-1:
+#           if len(prev_shapes)>2: att_before=True #cur_shape="ـ"+w0
+#         else:
+#           if len(prev_shapes)>2: att_before=True # cur_shape="ـ"+w0
+#           if len(possible_shapes)>2:  att_after=True
+#             #cur_shape=cur_shape+"ـ"
+#         prev_shapes=possible_shapes
+#         cur_shape=w0
+#         if att_before: cur_shape="ـ"+cur_shape
+#         if att_after: cur_shape=cur_shape+"ـ"
+#         self.word_letter_shapes_plain.append(cur_shape)
+#         #print(cur_shape)
+        
+#         cur_shape=ch
+#         if att_before: cur_shape="ـ"+cur_shape
+#         if att_after: cur_shape=cur_shape+"ـ"
+#         self.word_letter_shapes.append(cur_shape)
+    
+#       #get the sound
+
+#       prev_romanized=""
+#       cur_word_romanized_chunks=[]
+#       for ch_i,ch in enumerate(self.chunks):
+#         ch=ch.strip("ـ")
+#         found=self.romanize_dict.get(ch[0],ch[0])
+#         first=found[0]
+#         try:
+#           if ch_i==0 and found[1]!="": first=found[1]
+#         except:
+#           print("error","found",found,ch_i,ch,word0)
+#           continue
+
+#         if first=="q" and lang=="ega" and not "q" in self.word_params: first="2"
+#         #first=self.romanize_dict[ch[0]][0]
+        
+#         #cur_romanized_chunk=first
+#         dia0="".join([self.romanize_dict.get(v,["",""])[0] for v in ch[1:]]) #convert the diacritics part of the chunk
+#         has_shaddah=False
+#         if "x" in dia0: has_shaddah=True #in the conversion sheet, shadda is converted to x
+#         dia0=dia0.replace("x","")
+#         if dia0 and found[1]!="": first=found[1] 
+#         if ch_i>0 and "2" in found[0]: first="2" #imra2ah
+
+#         if prev_romanized.endswith("a") and ch[0] in "وي" and found[1]!="": first=found[1]
+#         if has_shaddah and found[1]!="": first=found[1]
+
+#         cur_romanized_chunk=first+dia0
+#         if has_shaddah: cur_romanized_chunk=first+cur_romanized_chunk #shaddah
+
+#         if ch_i==0 and cur_romanized_chunk=="aa" and ch[0]!="آ":  cur_romanized_chunk="a" #cheap hacks to fix certain fringe cases
+#         if cur_romanized_chunk=="ai":  cur_romanized_chunk="i"
+#         if cur_romanized_chunk=="ii":  cur_romanized_chunk="i"
+#         if cur_romanized_chunk=="au":  cur_romanized_chunk="u"
+#         if cur_romanized_chunk=="aan":  cur_romanized_chunk="an"
+
+#         self.romanized_chunks.append(cur_romanized_chunk)
+#         prev_romanized=cur_romanized_chunk
+#       #fine tuning the chunk sequence to avoid double vowels and make sure taa2 marbooTah is preceded vy fat7ah
+#       temp_romanized_chunks=[]
+#       for s_i, rom in enumerate(self.romanized_chunks):
+#         cur_ar_chunk=self.chunks[s_i]
+#         next_rom_chunk=""
+#         next_ar_chunk=""
+#         if s_i<len(self.romanized_chunks)-1:
+#           next_rom_chunk=self.romanized_chunks[s_i+1]
+#           next_ar_chunk=self.chunks[s_i+1]
+#         #if rom[0]=="q" and lang=="ega" and not "q" in self.word_params: rom="2"+rom 
+#         if rom in ["y","ee"] and lang=="ega" and "i" in self.word_params: rom="ai"
+#         if rom in ["w","oo"] and lang=="ega" and "o" in self.word_params: rom="oa"
+#         if next_rom_chunk=="aa": 
+#           if rom=="oo": rom="w"
+#           if rom=="ee": rom="y"
+#           rom=rom.rstrip("a")
+#         if next_rom_chunk=="ee": 
+#           if rom=="ee": rom="y"
+#           if rom=="oo": rom="w"
+#           rom=rom.rstrip("ei")
+#         if next_rom_chunk=="oo": 
+#           rom=rom.rstrip("ou")
+#         if next_ar_chunk=="ة": rom=rom.rstrip("a")+"a"
+#         temp_romanized_chunks.append(rom)
+
+
+
+#       self.romanized_chunks+=temp_romanized_chunks
+      
+#       for s_i, rom in enumerate(self.romanized_chunks):
+#         next_chunk=""
+#         if s_i<len(self.romanized_chunks)-1: next_chunk=self.romanized_chunks[s_i+1]
+#         # if rom.endswith("i") and next_chunk=="ee": rom=rom[:-1] #avoid iee for kasrah before yaa2
+#         # if rom.endswith("u") and next_chunk=="oo": rom=rom[:-1]
+#         # if rom.endswith("a") and next_chunk=="aa": rom=rom[:-1]
+#         self.romanized+=rom
+
+#       if w_i<len(all_words)-1:
+#         self.chunks.append(" ")
+#         self.word_letter_shapes_plain.append(" ")
+#         self.word_letter_shapes.append(" ")
+#         self.romanized_chunks.append(" ")
+#         self.romanized+=" "
+#         self.plain+=" "
+
+class analyze_ar_text: #analyze a word with diacritics in different ways
+  def __init__(self,text0,shape_dict=ar_shape_dict,romanize_dict=romanization_dict,lang="msa",exclude_waaw_jamaa3ah=True):
+    self.chunks=[]
+    self.word_letter_shapes_plain=[]
+    self.word_letter_shapes=[]
+    self.romanized_chunks=[]
+    self.romanized=""
+    self.plain=""
+    all_words=text0.split(" ")
+    for w_i,cur_word in enumerate(all_words)
+      obj0=analyze_ar_word(cur_word,shape_dict,romanize_dict,lang,exclude_waaw_jamaa3ah)
+      self.chunks.extend(obj0.chunks)
+      self.word_letter_shapes_plain.extend(obj0.word_letter_shapes_plain)
+      self.romanized_chunks.extend(obj0.romanized_chunks)
+      self.romanized+=obj0.romanized
+      self.plain+=obj0.plain
+      if w_i<len(all_words)-1:
+        self.chunks.append(" ")
+        self.word_letter_shapes_plain.append(" ")
+        self.word_letter_shapes.append(" ")
+        self.romanized_chunks.append(" ")
+        self.romanized+=" "
+        self.plain+=" "
+
+
+
 class analyze_ar_word: #analyze a word with diacritics in different ways
   def __init__(self,word0,shape_dict=ar_shape_dict,romanize_dict=romanization_dict,lang="msa",exclude_waaw_jamaa3ah=True):
     word_split=word0.split("_")
@@ -203,8 +381,6 @@ class analyze_ar_word: #analyze a word with diacritics in different ways
       if next_ar_chunk=="ة": rom=rom.rstrip("a")+"a"
       temp_romanized_chunks.append(rom)
 
-
-
     self.romanized_chunks=temp_romanized_chunks
     self.romanized=""
     for s_i, rom in enumerate(self.romanized_chunks):
@@ -214,100 +390,6 @@ class analyze_ar_word: #analyze a word with diacritics in different ways
       # if rom.endswith("u") and next_chunk=="oo": rom=rom[:-1]
       # if rom.endswith("a") and next_chunk=="aa": rom=rom[:-1]
       self.romanized+=rom
-
-# class analyze_ar_word: #analyze a word with diacritics in different ways
-#   def __init__(self,word0,shape_dict0=ar_shape_dict,romanize_dict0=romanization_dict,exclude_waaw_jamaa3ah=True):
-#     self.word=word0
-#     self.word1=word0
-#     if exclude_waaw_jamaa3ah and self.word1.endswith("وا"):self.word1=self.word1[:-1]
-#     self.shape_dict=shape_dict0
-#     self.romanize_dict=romanize_dict0
-#     #first split the word into chunks, based on diacritics
-#     self.chunks=[]
-#     new_chunk=""
-#     self.plain=""
-#     for w0 in self.word1:
-#       if w0.isalpha() or w0==" ":
-#         self.plain+=w0
-#         if  new_chunk!="": self.chunks.append(new_chunk)
-#         new_chunk=w0 
-#       else: new_chunk+=w0
-#     if  new_chunk!="": self.chunks.append(new_chunk)
-#     #get the shape
-#     self.word_letter_shapes_plain=[]
-#     self.word_letter_shapes=[]
-#     prev_shapes=[]
-    
-#     #get the shapes
-#     for i0,ch in enumerate(self.chunks):
-#       att_before,att_after=False,False #attach before and attach after
-#       w0=ch[0]
-#       cur_shape=w0
-#       possible_shapes=shape_dict0.get(w0,[])
-
-#       if i0==0 and len(possible_shapes)>2: att_after=True #cur_shape=w0+"ـ"
-#       elif i0==len(self.chunks)-1:
-#         if len(prev_shapes)>2: att_before=True #cur_shape="ـ"+w0
-#       else:
-#         if len(prev_shapes)>2: att_before=True # cur_shape="ـ"+w0
-#         if len(possible_shapes)>2:  att_after=True
-#           #cur_shape=cur_shape+"ـ"
-#       prev_shapes=possible_shapes
-#       cur_shape=w0
-#       if att_before: cur_shape="ـ"+cur_shape
-#       if att_after: cur_shape=cur_shape+"ـ"
-#       self.word_letter_shapes_plain.append(cur_shape)
-#       #print(cur_shape)
-      
-#       cur_shape=ch
-#       if att_before: cur_shape="ـ"+cur_shape
-#       if att_after: cur_shape=cur_shape+"ـ"
-#       self.word_letter_shapes.append(cur_shape)
-  
-#     #get the sound
-#     self.romanized_chunks=[]
-#     prev_romanized=""
-#     for ch_i,ch in enumerate(self.chunks):
-#       found=self.romanize_dict.get(ch[0],ch[0])
-#       first=found[0]
-#       if ch_i==0 and found[1]!="": first=found[1]
-#       #first=self.romanize_dict[ch[0]][0]
-      
-#       #cur_romanized_chunk=first
-#       dia0="".join([self.romanize_dict.get(v,["",""])[0] for v in ch[1:]]) #convert the diacritics part of the chunk
-#       has_shaddah=False
-#       if "x" in dia0: has_shaddah=True #in the conversion sheet, shadda is converted to x
-#       dia0=dia0.replace("x","")
-#       if dia0 and found[1]!="": first=found[1] 
-#       if ch_i>0 and "2" in found[0]: first="2" #imra2ah
-
-#       if prev_romanized.endswith("a") and ch[0] in "وي" and found[1]!="": first=found[1]
-#       if has_shaddah and found[1]!="": first=found[1]
-
-#       cur_romanized_chunk=first+dia0
-#       if has_shaddah: cur_romanized_chunk=first+cur_romanized_chunk #shaddah
-
-#       if ch_i==0 and cur_romanized_chunk=="aa" and ch[0]!="آ":  cur_romanized_chunk="a" #cheap hacks to fix certain fringe cases
-#       if cur_romanized_chunk=="ai":  cur_romanized_chunk="i"
-#       if cur_romanized_chunk=="ii":  cur_romanized_chunk="i"
-#       if cur_romanized_chunk=="au":  cur_romanized_chunk="u"
-#       if cur_romanized_chunk=="aan":  cur_romanized_chunk="an"
-
-
-
-
-#       self.romanized_chunks.append(cur_romanized_chunk)
-#       #print(cur_romanized_chunk)
-#       prev_romanized=cur_romanized_chunk
-#     self.romanized=""
-#     for s_i, rom in enumerate(self.romanized_chunks):
-#       next_chunk=""
-#       if s_i<len(self.romanized_chunks)-1: next_chunk=self.romanized_chunks[s_i+1]
-#       if rom.endswith("i") and next_chunk=="ee": rom=rom[:-1] #avoid iee for kasrah before yaa2
-#       if rom.endswith("u") and next_chunk=="oo": rom=rom[:-1]
-#       if rom.endswith("a") and next_chunk=="aa": rom=rom[:-1]
-#       self.romanized+=rom
-
 
 def rstrip_if(word,to_strip):
   if word.endswith(to_strip): word=word[:-len(to_strip)]
