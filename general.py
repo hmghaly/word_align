4a2aacc23837491d0c9767e4581fbff3d2938d4b
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-07-27 12:57:40 -0400

    Add files via upload

--- general.py
+++ general.py
@@ -707,7 +707,7 @@ def get_file_loc_ratio(ratio,fpath): #get the line start location corresponding 
 
 #read the file from certain percentage to certain percentage - useful for navigating large files
 #and also for splitting files to training/dev/test portions
-def process_file_from_to(fpath,to_ratio=None,from_ratio=0,from_loc=None,to_loc=None): #process file line by line from start percentage/ratio (e.g. 0%) to an end ratio (e.g. 80%)
+def read_file_from_to(fpath,to_ratio=None,from_ratio=0,from_loc=None,to_loc=None): #process file line by line from start percentage/ratio (e.g. 0%) to an end ratio (e.g. 80%)
   file_size=os.stat(fpath).st_size
   if from_loc==None:
     if from_ratio==0: from_loc=0
