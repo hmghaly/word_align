d2588bae1e8462e5e25984a93dc8a7af52fd5acd
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-09-15 09:26:58 -0400

    Add files via upload

--- cat_utils.py
+++ cat_utils.py
@@ -1390,8 +1390,8 @@ def pre_edit_docx(docx_fpath,nn_model_obj,first_token_dict,pred_threshold=0.5,pr
     else: pre_edit_out_tokens,valid_repl=pre_edit(original0,nn_model_obj,first_token_dict,pred_threshold) #if we want to pre-edit the final
 
     pre_edit_out_str=general.de_tok2str(pre_edit_out_tokens)
-    pre_edit_html=get_edit_html(original_tokens,pre_edit_out_tokens,class_name="automatic")
-    token_edited_html=get_edit_html(original_tokens,final_tokens,class_name="human")
+    pre_edit_html=get_edit_html(original_tokens,pre_edit_out_tokens,main_class_name="automatic")
+    token_edited_html=get_edit_html(original_tokens,final_tokens,main_class_name="human")
     new_edit_pre_edit_list.append((para_path0,original0,final0,edited0,token_edited_html,pre_edit_out_str,pre_edit_html))
     all_repl_inst_list.extend(valid_repl)
   return new_edit_pre_edit_list,all_repl_inst_list
@@ -1429,8 +1429,8 @@ def analyze_pre_edit_docx(docx_fpath,nn_model_obj,first_token_dict,pred_threshol
 
     #print("valid_compare_repl_spans_dict",valid_compare_repl_spans_dict)
     pre_edit_out_str=general.de_tok2str(pre_edit_out_tokens)
-    pre_edit_html=get_edit_html(original_tokens,pre_edit_out_tokens,class_name="automatic")
-    token_edited_html=get_edit_html(original_tokens,final_tokens,class_name="human")
+    pre_edit_html=get_edit_html(original_tokens,pre_edit_out_tokens,main_class_name="automatic")
+    token_edited_html=get_edit_html(original_tokens,final_tokens,main_class_name="human")
     new_edit_pre_edit_list.append((para_path0,original0,final0,edited0,token_edited_html,pre_edit_out_str,pre_edit_html))
     #new_edit_pre_edit_list.append((para_path0,original0,final0,edited0,token_edited_html,pre_edit_out_str,pre_edit_html))
     for model_repl_inst in valid_repl:
