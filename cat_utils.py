8c8061ff69df20ea34c74800ca16aa65994cab14
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-10-09 08:07:29 -0400

    Add files via upload

--- cat_utils.py
+++ cat_utils.py
@@ -1401,7 +1401,7 @@ def para2sents(text):
 
 
 #============ Pre-editing pipeline ========================
-def pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5): #pre-edit a sentence given the first token dict and a neural network model object with prediction
+def pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5,freq_threshold=None): #pre-edit a sentence given the first token dict and a neural network model object with prediction
   sent_tokens0=general.add_padding(general.tok(sent_str))
   span_repl_items=extract_repl_instances(sent_tokens0,[],first_token_dict)
   new_items=[]
@@ -1414,15 +1414,15 @@ def pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5): #pre-ed
     #if wt0<pred_threshold: continue
     item0["wt"]=wt0
     new_items.append(item0)
-  if nn_model_obj!=None: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="wt",min_wt=pred_threshold)
-  else: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="freq",min_wt=0)
+  if nn_model_obj!=None: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="wt",min_wt=pred_threshold,min_freq=freq_threshold)
+  else: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="freq",min_wt=0,min_freq=freq_threshold)
   if pre_edited_sent_tokens[0]=="<s>": pre_edited_sent_tokens=pre_edited_sent_tokens[1:]
   if pre_edited_sent_tokens[-1]=="</s>": pre_edited_sent_tokens=pre_edited_sent_tokens[:-1]
   return pre_edited_sent_tokens,valid_repl
 
-def pre_edit_html(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5):
+def pre_edit_html(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5,freq_threshold=None):
   original_tokens=general.tok(sent_str)
-  pre_edited_sent_tokens,valid_repl=pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5)
+  pre_edited_sent_tokens,valid_repl=pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=pred_threshold,freq_threshold=freq_threshold)
   pre_edit_html=get_edit_html(original_tokens,pre_edited_sent_tokens,main_class_name="automatic")
   return pre_edit_html, valid_repl
 
