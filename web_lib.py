5bf4f5b98a78d0a97a15faf2df96dd00a3df0333
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-12-02 09:20:25 -0500

    Add files via upload

--- web_lib.py
+++ web_lib.py
@@ -625,3 +625,26 @@ def get_emails(html_content):
     if not check: continue
     valid_emails.append(em0)
   return valid_emails
+
+def get_page_text(page_url):
+  text=get_page_content(page_url)
+  # (REMOVE <SCRIPT> to </script> and variations)
+  pattern = r'<[ ]*script.*?\/[ ]*script[ ]*>'  # mach any char zero or more times
+  text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+  # (REMOVE HTML <STYLE> to </style> and variations)
+  pattern = r'<[ ]*style.*?\/[ ]*style[ ]*>'  # mach any char zero or more times
+  text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+  # (REMOVE HTML <META> to </meta> and variations)
+  pattern = r'<[ ]*meta.*?>'  # mach any char zero or more times
+  text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+  # (REMOVE HTML COMMENTS <!-- to --> and variations)
+  pattern = r'<[ ]*!--.*?--[ ]*>'  # mach any char zero or more times
+  text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+  # (REMOVE HTML DOCTYPE <!DOCTYPE html to > and variations)
+  pattern = r'<[ ]*\![ ]*DOCTYPE.*?>'  # mach any char zero or more times
+  text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+  return text
