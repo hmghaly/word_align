74219f39ce400f074704585ddcae17cd1ca518c4
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-01-17 03:21:09 -0500

    Add files via upload

--- web_lib.py
+++ web_lib.py
@@ -319,7 +319,7 @@ def create_open_tag(tag_name0,tag_attrs0={},self_closing=False):
 
 def read_page(url, timeout0=5): #return requests obj
   op=requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, timeout=timeout0, allow_redirects=True)
-  op.encoding = op.apparent_encoding
+  op.encoding = "utf-8" #op.apparent_encoding
   return op
 #   status_code=op.status_code
 #   html_content=op.text
