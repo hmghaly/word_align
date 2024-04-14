f98d6b624e62644d80f5fbc8a966ce88e91dd19d
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-04-14 18:34:09 -0400

    Add files via upload

--- mongo_utils.py
+++ mongo_utils.py
@@ -3,7 +3,7 @@ import pymongo
 import os, json, time
 from itertools import groupby
 
-def mongo_find(query,collection,page_i=1,n_limit=10):
+def mongo_find(query,collection,sort_key=None, page_i=1,n_limit=10):
     t0=time.time()
     if page_i<1: page_i=1
 
@@ -15,7 +15,9 @@ def mongo_find(query,collection,page_i=1,n_limit=10):
     display_page_i=page_i
 
     skip_val=actual_page_i*n_limit
-    cursor = collection.find(query, limit=n_limit).skip(skip_val) #enter the query
+    if sort_key==None: cursor = collection.find(query, limit=n_limit).skip(skip_val) #enter the query
+    else: cursor = collection.find(query, sort={sort_key:-1}, limit=n_limit).skip(skip_val) #enter the query
+        
     res_list=list(cursor)
     t1=time.time()
     out_dict={}
