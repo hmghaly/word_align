4b376a3395567696d7c1868267bf1a850cf5e9e3
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-07-11 18:37:49 -0400

    Add files via upload

---
+++ ai_utils.py
@@ -0,0 +1,12 @@
+import requests
+
+model="gpt-3.5-turbo"
+def chat_with_chatgpt(prompt,api_key,max_tokens=100):
+    res = requests.post(f"https://api.openai.com/v1/chat/completions",
+          headers = {
+              "Content-Type": "application/json",
+              "Authorization": f"Bearer {api_key}"
+          },
+          json={"model": model,"messages": [{"role": "user", "content": prompt}], "max_tokens": max_tokens
+          }).json()
+    return res#.choices[0].text
