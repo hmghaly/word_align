ddbb7c851c69953746c61ab261e7ec71c1081a58
Hussein Ghaly <hmghaly@users.noreply.github.com>
2026-03-01 05:02:36 -0500

    Add files via upload

--- general.py
+++ general.py
@@ -899,7 +899,9 @@ def get_tag_loc_pairs(tokens,params={}):
     if is_open_tag==is_close_tag==False:
       loc_i+=1
   not_well_formed.extend(cur_open_tags)
-  well_formed_ratio=len(cur_tag_pairs)/(len(cur_tag_pairs)+len(not_well_formed))
+  denom=(len(cur_tag_pairs)+len(not_well_formed))
+  if denom==0: well_formed_ratio=0
+  else: well_formed_ratio=len(cur_tag_pairs)/denom
   output={"tag_pairs":cur_tag_pairs,"not_well_formed":not_well_formed,"well_formed_ratio":well_formed_ratio}
   return output
 
